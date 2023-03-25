a51f1541d4ba47c28fa95c36e111205a3cdee3fc
Abdelrahman <bodyronaldo30@gmail.com>
2023-03-25 19:15:06 +0200

    task 0

---
+++ 0-print_func.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+  * _printf - produces output according to format
+  *
+  * @format: the format to be printed
+  *
+  * Return: the number of characters
+  */
+int _printf(const char *format, ...)
+{
+	int i;
+	int str_len = 0;
+	va_list ap;
+
+	va_start(ap, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					{
+						char ch = va_arg(ap, int);
+						char *c = &ch;
+
+						write(1, c, 1);
+						break;
+					}
+
+				case 's':
+					{
+						char *str = va_arg(ap, char *);
+
+						str_len = strlen(str);
+						write(1, str, str_len);
+						break;
+					}
+			}
+			continue;
+		}
+		write(1, format + i, 1);
+	}
+	va_end(ap);
+	return (str_len + i);
+}
---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+int _printf(const char *format, ...);
+#endif
